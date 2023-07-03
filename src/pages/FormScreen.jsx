ac0fdf37e6002cd5be4df3738a84ae0cbd70c623
haphong463 <111565442+haphong463@users.noreply.github.com>
2023-07-03 23:19:17 +0700

    'push'

---
+++ src/pages/FormScreen.jsx
@@ -0,0 +1,31 @@
+import { Form, Input } from "antd";
+import React from "react";
+
+const FormScreen = () => {
+  return (
+    <div>
+      <Form>
+        <div>
+          <div className="padding-24">asdsad</div>
+          <div className="padding-24"></div>
+          <div className="padding-24"></div>
+
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+
+        <Form.Item>
+          <Input />
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default FormScreen;
